2425911fa8e1d88e10a5056ef50859e02291fa03
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-03-24 18:22:23 +0100

    Repair events

--- src/Consultations/CommonConsultationVariables.php
+++ src/Consultations/CommonConsultationVariables.php
@@ -2,7 +2,7 @@
 
 namespace EscolaLms\TemplatesSms\Consultations;
 
-use EscolaLms\Consultations\Models\ConsultationTerm;
+use EscolaLms\Consultations\Models\ConsultationUserPivot;
 use EscolaLms\Core\Models\User;
 use EscolaLms\Templates\Events\EventWrapper;
 use EscolaLms\TemplatesSms\Core\SmsVariables;
@@ -45,6 +45,6 @@ abstract class CommonConsultationVariables extends SmsVariables
 
     public static function assignableClass(): ?string
     {
-        return ConsultationTerm::class;
+        return ConsultationUserPivot::class;
     }
 }
